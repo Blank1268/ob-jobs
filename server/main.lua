db612630b87eb4961798bdd39b2f6cd871671685
jake deroche <Blank1268@hotmail.com>
2026-07-01 17:41:33 -0400

    Created OB Jobs Framework skeleton

---
+++ server/main.lua
@@ -0,0 +1,7 @@
+CreateThread(function()
+    Wait(1000)
+
+    print("^2====================================")
+    print("^2      OB Jobs Framework Loaded")
+    print("^2====================================")
+end)
---
+++ shared/jobs.lua
@@ -0,0 +1,15 @@
+OBJobs = {}
+
+OBJobs.Jobs = {}
+
+function OBJobs:RegisterJob(job)
+    -- We'll build this next
+end
+
+function OBJobs:GetJob(jobId)
+    -- We'll build this next
+end
+
+function OBJobs:GetJobs()
+    return self.Jobs
+end
